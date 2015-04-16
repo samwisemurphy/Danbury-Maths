ca8c8e4c1fa035b4af5f65a21bb8bc26c4baeefb
samwisemurphy <samuelmurphy@Samuels-MacBook-Pro.local>
2015-04-16 09:06:32 +0100

    first-upload
    Adding all the files for tutor project to Git

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Danbury Maths Tutor</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+<ul class="nav nav-tabs nav-justified" id="shit">
+	<li><a href="#">Home</a></li>
+	<li><a href="#">About Me</a></li>
+	<li><a href="#">Testimonials</a></li>
+	<li><a href="#">Contact Me</a></li>
+	<li><a href="#">Blog</a></li>
+</ul>
+
+<div class="top-image">
+	
+	<img src="white.png" alt="" id="fuck">
+
+</div>
+
+<div class="jumbotron center img">
+	<h1>Hello World</h1>
+	<p>Well fucking welcome to my website</p>
+</div>	
+
+<div class="container">
+	<div class="row">
+		<div class="col-lg-4">
+			fuck 
+		</div>
+		<div class="col-lg-4">
+			this
+		</div>
+		<div class="col-lg-4">
+			shit
+		</div>
+	</div>
+</div>
+
+
+
+
+
+
+
+	<script src="js/jquery.min.js" type="text/javascript"></script>  
+	<script src="js/bootstrap.js" type="text/javascript"></script>
+	<script src="js/bootstrap.min.js" type="text/javascript"></script> 
+</body>
+</html>
