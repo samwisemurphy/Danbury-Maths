4a303258601af91760bd02ef379c886e0ade7ce6
samwisemurphy <samuelmurphy@Samuels-MacBook-Pro.local>
2015-04-20 09:11:46 +0100

    Danbury-update
    Changing of  header image and a few other bits

--- index.php
+++ index.php
@@ -7,38 +7,126 @@
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
-<ul class="nav nav-tabs nav-justified" id="shit">
-	<li><a href="#">Home</a></li>
-	<li><a href="#">About Me</a></li>
-	<li><a href="#">Testimonials</a></li>
-	<li><a href="#">Contact Me</a></li>
-	<li><a href="#">Blog</a></li>
-</ul>
 
-<div class="top-image">
-	
-	<img src="white.png" alt="" id="fuck">
-
-</div>
-
-<div class="jumbotron center img">
-	<h1>Hello World</h1>
-	<p>Well fucking welcome to my website</p>
-</div>	
-
-<div class="container">
-	<div class="row">
-		<div class="col-lg-4">
-			fuck 
+<div class="navbar navbar-inverse custom-nav smoke">
+	<div class="container">
+		<div class="title-img">
+			<a  class="navbar-brand logo-pad" href="#">
+				<img  src="Logos/originals/circle-me.png" alt="">
+			</a>
 		</div>
-		<div class="col-lg-4">
-			this
-		</div>
-		<div class="col-lg-4">
-			shit
+		<div class="navbar-collapse">
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#" class="title-links">Home</a></li>
+				<li><a href="#" class="title-links">About me</a></li>
+				<li><a href="#" class="title-links">Testimonials</a></li>
+				<li><a href="#" class="title-links">Tutoring info</a></li>
+				<li><a href="#" class="title-links">Contact me</a></li>	
+			</ul>
 		</div>
 	</div>
 </div>
+
+
+<div class="top-image">
+	<h1 id="dmt">Danbury Maths Tutor</h1>
+</div>
+	
+<div class="container">
+	<div class="row">
+		<div class="col-md-12 text-center">
+			<h1>About Me</h1>
+		</div>
+	</div>
+	<div class="row text-center sam-weston padding-top-20 border-top">
+		<div class="col-md-4">
+			<img class="profile-image" src="profile.jpg" alt="profile" height="200">
+			<div class="about">
+				<p>Samuel Weston</p>
+				<p>Maths Tutor</p>
+				<p>Warick University Grad</p>
+				<p>Full time tutor</p>
+			</div>
+		</div>
+		<div class="col-md-8 padding-top-20 about-text text-left">
+			<h2>Who am I?</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam interdum rhoncus elit sit amet sollicitudin. Suspendisse non justo euismod, semper velit sit amet, mollis ligula. Mauris mollis sit amet urna id lobortis. Suspendisse consectetur elementum mattis. Sed vel turpis tempus, mollis lectus at, elementum est. Suspendisse potenti. Etiam neque dui, venenatis et venenatis a, hendrerit ac nibh. Donec fermentum ligula nunc, ut vestibulum lacus sodales ac. Phasellus odio felis, vestibulum in eros dapibus, facilisis tristique sem. Donec eleifend tincidunt volutpat.</p>
+			<h2>What I do</h2>
+			<p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis non mauris non tortor sodales varius eu a neque. In id fermentum magna. Curabitur scelerisque aliquet vestibulum. Sed eu nulla consectetur, consequat elit ac, varius mi. Maecenas egestas metus eu sem ultrices, in faucibus nulla suscipit. Nulla molestie felis sed volutpat aliquet. Vestibulum ante justo, dictum id venenatis nec, venenatis sed ligula. Vestibulum eu sem ut tortor convallis ultrices</p>
+		</div>
+	</div>
+	<div class="row ">
+		<div class="col-md-12 text-center margin-bottom-20">
+			<h1>Testimonials</h1>
+		</div>
+	</div>
+
+	<div class="row padding-top-40 smoke testimonials-top">
+		<div class="col-md-3">
+			<img class="profile-image" src="profile.jpg" alt="profile" height="160">
+		</div>
+		<div class="col-md-3">
+			<div class="bubble-2 margin-bottom-20"><div class="pointer-left"></div></div>
+		</div>
+		<div class="col-md-3">	
+			<img class="profile-image" src="profile.jpg" alt="profile" height="160">
+		</div>
+		<div class="col-md-3">
+			<div class="bubble-2 margin-bottom-20"><div class="pointer-left"></div></div>
+		</div>
+	</div>
+	
+	<div class="row margin-top-40 smoke soften testimonials-bot">
+		<div class="col-md-3">
+			<img class="profile-image" src="profile.jpg" alt="profile" height="160">
+		</div>
+		<div class="col-md-3">
+			<div class="bubble-2 margin-bottom-20"><div class="pointer-left"></div></div>
+		</div>
+		<div class="col-md-3">	
+			<img class="profile-image" src="profile.jpg" alt="profile" height="160">
+		</div>
+		<div class="col-md-3">
+			<div class="bubble-2 margin-bottom-20"><div class="pointer-left"></div></div>
+		</div>
+	</div>
+	
+	
+	
+
+	<div class="row ">
+		<div class="col-md-12 text-center margin-bottom-20">
+			<h1>Get in touch</h1>
+		</div>
+	</div>
+	<div class="row border-top smoke padding-bottom-20">
+		<div class="col-md-6">
+			<h2>Contact Details</h2>
+			<label for="name">Name:</label>
+			<div class="input-group input-group-lg">
+  				<input type="text" class="form-control" aria-describedby="sizing-addon1">
+			</div><br>
+			<label for="email">Email:</label>
+			<div class="input-group input-group-lg">
+  				<input type="text" class="form-control" aria-describedby="sizing-addon1">
+			</div><br>
+			<label for="phone">Phone:</label>
+			<div class="input-group input-group-lg">
+  				<input type="text" class="form-control" aria-describedby="sizing-addon1">
+			</div><br>
+		</div>
+		<div class="col-md-6">
+			<h2>Message</h2>
+			<textarea id="message"></textarea><br>
+			<input class="btn btn-default" type="submit" action="submit">
+		</div>
+	</div>
+	<div class="margin-bottom-big"></div>
+</div>
+
+
+
+
 
 
 
