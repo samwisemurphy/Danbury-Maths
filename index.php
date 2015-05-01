ce874f64e6b3050ab21bc1f7549d637627e6b407
samwisemurphy <samuelmurphy@Samuels-MacBook-Pro.local>
2015-05-01 08:36:51 +0100

    updates
    More changes

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 </head>
 <body>
 
-<div class="navbar navbar-inverse custom-nav smoke">
+<div class="navbar navbar-inverse custom-nav grey">
 	<div class="container">
 		<div class="title-img">
 			<a  class="navbar-brand logo-pad" href="#">
@@ -18,101 +18,111 @@
 		<div class="navbar-collapse">
 			<ul class="nav navbar-nav navbar-right">
 				<li><a href="#" class="title-links">Home</a></li>
-				<li><a href="#" class="title-links">About me</a></li>
+				<li><a href="#" class="title-links">About Me</a></li>
 				<li><a href="#" class="title-links">Testimonials</a></li>
-				<li><a href="#" class="title-links">Tutoring info</a></li>
-				<li><a href="#" class="title-links">Contact me</a></li>	
+				<li><a href="#" class="title-links">Tutoring Info</a></li>
+				<li><a href="#" class="title-links">Contact Me</a></li>	
 			</ul>
 		</div>
 	</div>
 </div>
 
-
-<div class="top-image">
-	<h1 id="dmt">Danbury Maths Tutor</h1>
+<div class="top-image margin-bottom-0 padding-bottom-0">
+	<img class="danbury-logo" src="Logos/originals/circle-logo.png" alt="danbury logo">
+	<h1 id="dmt" class="margin-top-0 padding-top-0">Danbury Maths Tutor</h1>
 </div>
-	
-<div class="container">
-	<div class="row">
-		<div class="col-md-12 text-center">
-			<h1>About Me</h1>
-		</div>
+
+
+<div class="test text-white padding-top-0 marg-r-0">
+	<div class="text-center text-white padding-top-0 marg-r-0">
+		<h1 id="pad" class="text-shadow padding-top-0 margin-top-0">About Me</h1>
 	</div>
-	<div class="row text-center sam-weston padding-top-20 border-top">
+</div>
+
+<div class="container">	
+	<div class="row text-center sam-weston padding-top-20 border-about-me margin-top-60 padding-top-20">
 		<div class="col-md-4">
 			<img class="profile-image" src="profile.jpg" alt="profile" height="200">
-			<div class="about">
+			<div class="smoke border-radius about mar-l-80">
 				<p>Samuel Weston</p>
 				<p>Maths Tutor</p>
 				<p>Warick University Grad</p>
 				<p>Full time tutor</p>
 			</div>
 		</div>
-		<div class="col-md-8 padding-top-20 about-text text-left">
+		<div class="col-md-8 padding-top-20 text-left smoke border-radius">
+			<div class="border-small-about">
 			<h2>Who am I?</h2>
 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam interdum rhoncus elit sit amet sollicitudin. Suspendisse non justo euismod, semper velit sit amet, mollis ligula. Mauris mollis sit amet urna id lobortis. Suspendisse consectetur elementum mattis. Sed vel turpis tempus, mollis lectus at, elementum est. Suspendisse potenti. Etiam neque dui, venenatis et venenatis a, hendrerit ac nibh. Donec fermentum ligula nunc, ut vestibulum lacus sodales ac. Phasellus odio felis, vestibulum in eros dapibus, facilisis tristique sem. Donec eleifend tincidunt volutpat.</p>
+			</div>
 			<h2>What I do</h2>
 			<p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis non mauris non tortor sodales varius eu a neque. In id fermentum magna. Curabitur scelerisque aliquet vestibulum. Sed eu nulla consectetur, consequat elit ac, varius mi. Maecenas egestas metus eu sem ultrices, in faucibus nulla suscipit. Nulla molestie felis sed volutpat aliquet. Vestibulum ante justo, dictum id venenatis nec, venenatis sed ligula. Vestibulum eu sem ut tortor convallis ultrices</p>
 		</div>
 	</div>
-	<div class="row ">
-		<div class="col-md-12 text-center margin-bottom-20">
-			<h1>Testimonials</h1>
-		</div>
+</div> <!-- END OF CONTAINER -->	
+<div class="test text-white">
+	<div class="text-center margin-bottom-40 padding-bottom-20">
+		<h1>Testimonials</h1>
 	</div>
+</div>
 
-	<div class="row padding-top-40 smoke testimonials-top">
-		<div class="col-md-3">
+<div class="container margin-bottom-20 margin-top-20">
+	<div class="row padding-top-40 padding-bottom-40 black margin-bottom-0 border-radius">
+		
+		<div class="col-md-2">
 			<img class="profile-image" src="profile.jpg" alt="profile" height="160">
 		</div>
 		<div class="col-md-3">
-			<div class="bubble-2 margin-bottom-20"><div class="pointer-left"></div></div>
+			<div class="bubble-2 margin-bottom-20 pad-l-0 mar-l-0"><p class="pad-left-4">"Proin fermentum libero sed metus varius, eget consequat tellus interdum. Aenean nec feugiat mauris, sed iaculis nisi. Sed neque sem, scelerisque quis sapien non, ornare pharetra urna. Quisque eu orci leo."</p>
+<div class="pointer-left"></div></div>
 		</div>
-		<div class="col-md-3">	
+		
+		<div class="col-md-2">	
 			<img class="profile-image" src="profile.jpg" alt="profile" height="160">
 		</div>
 		<div class="col-md-3">
-			<div class="bubble-2 margin-bottom-20"><div class="pointer-left"></div></div>
+			<div class="bubble-2 margin-bottom-10"><p class="pad-left-4">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. In posuere malesuada semper. Fusce auctor congue volutpat. Proin ut erat ut sem iaculis finibus. Nunc ultrices finibus commodo. Integer ut orci suscipit."</p><div class="pointer-left"></div></div>
 		</div>
 	</div>
-	
-	<div class="row margin-top-40 smoke soften testimonials-bot">
-		<div class="col-md-3">
+	<div class="row  marg-top-10 padding-top-40 padding-bottom-40 soften black border-radius">
+		<div class="col-md-2 marg-r-0">
 			<img class="profile-image" src="profile.jpg" alt="profile" height="160">
 		</div>
 		<div class="col-md-3">
-			<div class="bubble-2 margin-bottom-20"><div class="pointer-left"></div></div>
+			<div class="bubble-2 margin-bottom-20"><p class="pad-left-4">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elit ante, vestibulum ac turpis vel, euismod bibendum diam. Phasellus sed facilisis odio. Donec ullamcorper leo sapien, at facilisis dolor porttitor eu."</p><div class="pointer-left"></div></div>
 		</div>
-		<div class="col-md-3">	
+		<div class="col-md-2">	
 			<img class="profile-image" src="profile.jpg" alt="profile" height="160">
 		</div>
 		<div class="col-md-3">
-			<div class="bubble-2 margin-bottom-20"><div class="pointer-left"></div></div>
+			<div class="bubble-2 margin-bottom-40"><p class="pad-left-4">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et justo dapibus, venenatis orci at, varius ante. Ut hendrerit diam quis semper efficitur. Nulla et libero lorem. Duis pellentesque, diam sit."</p><div class="pointer-left"></div></div>
 		</div>
 	</div>
-	
+</div>	<!-- END OF CONTAINER -->
 	
 	
 
-	<div class="row ">
-		<div class="col-md-12 text-center margin-bottom-20">
-			<h1>Get in touch</h1>
-		</div>
+<div class="test text-white">
+	<div class=" text-center margin-bottom-20">
+		<h1>Get in touch</h1>
 	</div>
-	<div class="row border-top smoke padding-bottom-20">
+</div>
+
+<div class="container margin-top-20">
+	<div class="row border-top smoke padding-bottom-20 border-radius text-black">
 		<div class="col-md-6">
 			<h2>Contact Details</h2>
 			<label for="name">Name:</label>
 			<div class="input-group input-group-lg">
-  				<input type="text" class="form-control" aria-describedby="sizing-addon1">
+					<input type="text" class="form-control" aria-describedby="sizing-addon1">
 			</div><br>
 			<label for="email">Email:</label>
 			<div class="input-group input-group-lg">
-  				<input type="text" class="form-control" aria-describedby="sizing-addon1">
+					<input type="text" class="form-control" aria-describedby="sizing-addon1">
 			</div><br>
 			<label for="phone">Phone:</label>
 			<div class="input-group input-group-lg">
-  				<input type="text" class="form-control" aria-describedby="sizing-addon1">
+					<input type="text" class="form-control" aria-describedby="sizing-addon1">
 			</div><br>
 		</div>
 		<div class="col-md-6">
@@ -121,14 +131,22 @@
 			<input class="btn btn-default" type="submit" action="submit">
 		</div>
 	</div>
-	<div class="margin-bottom-big"></div>
+</div> <!-- END OF CONTAINER-->	
+<div class="margin-top-40 footer-colour padding-top-20">
+	<div class="footer col-md-6">
+		<a href="#" class="#">Home</a>
+		<a href="#" class="#">About me</a>
+		<a href="#" class="#">Testimonials</a>	
+		<a href="#" class="#">Tutoring info</a>
+		<a href="#" class="#">Contact me</a>
+		<a href="#" class="#">Terms</a>	
+	</div>
+	<div class="col-md-6 footer-ad">
+		<p>Sam Westons address goes here</p>
+		<p>Telephon number here</p>
+		<p>Email adddresss</p>
+	</div>
 </div>
-
-
-
-
-
-
 
 
 
